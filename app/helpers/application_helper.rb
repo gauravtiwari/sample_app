d91b661b3324e34393f1afbd20f1546d8dc3b6e5
Gaurav Tiwari <g.tiwari@lancaster.ac.uk>
2011-06-24 02:22:24 +0100

    Added the paths to image link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
 		"#{base_title} | #{@title}"
 		end
 	end
+
+	def logo
+	image_tag("logo.png",:alt=>"My sample application",:height=>"100px",:title=>"Rails Sample application", :class=>"round")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+root :to => 'pages#home'
+match '/contact', :to=>'pages#contact'
+match '/about', :to=>'pages#about'
+match '/help', :to=>'pages#help'
 
-  get "pages/contact"
 
-get "pages/about"
+ #You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,11 +57,11 @@ get "pages/about"
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
+ 
+
+  # See how all your routes lay out with "rake routes" # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
